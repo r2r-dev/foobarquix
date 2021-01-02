69fba55c06cac58c1fb7447f15355d19b70fb363
r2r-dev <dev@r2r.sh>
2021-01-02 05:44:40 +0100

    add linters, coverage

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,20 +1,23 @@
-from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
 from app.asgi import get_application
+
 app = get_application()
 
 client = TestClient(app)
+
 
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 404
     assert response.json() == {"detail": "Not Found"}
 
+
 def test_ready():
     response = client.get("/ready")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
+
 
 def test_num():
     response = client.get("/33")
